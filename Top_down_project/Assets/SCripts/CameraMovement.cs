a588e20236ed2d59b77a4a0d9b74480a70d80b25
nikolay yotov <nikolay.yotov@abv.bg>
2015-11-28 15:44:53 +0000

    getting my first .cs files on Git save Pleas enter the commit message for your changes. Lines starting

---
+++ Top_down_project/Assets/SCripts/CameraMovement.cs
@@ -0,0 +1,102 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+	GameObject GameCamera;
+	int CameraMoveSpeed;
+	int cameraMaxXY=50;
+	int CameraRotDisLocation=400;
+	float startTime;
+	float journeyLength =100;
+	short non_inverse=1,inverse=0,negetor=1;// it is reversed to what i wanted but works
+	// negetor needed since after Inversing it didnt work properly for Ox
+
+	// Use this for initialization
+	void Start () {
+	
+		CameraMoveSpeed = 100;
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{   
+
+///////////////////// CAMERA END OF SCREEN MOVEMENT //////////////////////////
+				float distCovered = (Time.time - startTime) * CameraMoveSpeed;
+				float fracJourney = distCovered / journeyLength;
+				//	transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+				if (Input.mousePosition.x > Screen.width - 50 && 
+		            this.transform.position.x*non_inverse <cameraMaxXY &&
+		    (this.transform.position.z-CameraRotDisLocation)*inverse >-cameraMaxXY) {
+						this.transform.position = 
+		    Vector3.Lerp 
+           (this.transform.position,
+  			new Vector3 (this.transform.position.x + non_inverse*negetor *CameraMoveSpeed * Time.deltaTime,
+					     this.transform.position.y,
+					     this.transform.position.z + inverse* negetor * CameraMoveSpeed * Time.deltaTime),
+		    fracJourney);
+				}
+
+		else if (Input.mousePosition.x < 0 + 50 && 
+		         this.transform.position.x*non_inverse >-cameraMaxXY &&
+		         (this.transform.position.z-CameraRotDisLocation)*inverse <cameraMaxXY) {
+						this.transform.position = 
+		    Vector3.Lerp 
+           (this.transform.position,
+			new Vector3 (this.transform.position.x - non_inverse*negetor *CameraMoveSpeed * Time.deltaTime,
+					     this.transform.position.y,
+					     this.transform.position.z - inverse *negetor * CameraMoveSpeed * Time.deltaTime),
+			fracJourney);
+				}
+
+		if (Input.mousePosition.y > Screen.height - 50 && 
+		    (this.transform.position.x+CameraRotDisLocation)*inverse <cameraMaxXY &&
+		    (this.transform.position.z) *non_inverse <cameraMaxXY)
+						this.transform.position = 
+		    Vector3.Lerp 
+           (this.transform.position,
+				 new Vector3 (this.transform.position.x + inverse *CameraMoveSpeed * Time.deltaTime,
+				             this.transform.position.y,
+				             this.transform.position.z + non_inverse * CameraMoveSpeed * Time.deltaTime),
+		    fracJourney);
+
+		else if (Input.mousePosition.y < 0 + 50 &&
+		         (this.transform.position.x+CameraRotDisLocation)*inverse >-(cameraMaxXY) &&
+		         this.transform.position.z *non_inverse >-cameraMaxXY)
+						this.transform.position = 
+				Vector3.Lerp 
+				(this.transform.position,
+				 new Vector3 (this.transform.position.x - inverse *CameraMoveSpeed * Time.deltaTime,
+				             this.transform.position.y,
+				             this.transform.position.z - non_inverse * CameraMoveSpeed * Time.deltaTime),
+				 fracJourney);
+
+		////////////// CAMERA ROTATION BY 90 DEGREES //////
+				if (Input.GetKeyDown (KeyCode.D)) {
+						if (transform.eulerAngles != (new Vector3 (30, 90, 0))) {
+								transform.eulerAngles = new Vector3 (30, 90, 0);
+								transform.position = new Vector3 (this.transform.position.x - CameraRotDisLocation,
+			                                this.transform.position.y,
+			                                this.transform.position.z+CameraRotDisLocation );
+				inverse=1;
+				non_inverse=0;
+				negetor=-1;
+						}
+				}
+				if (Input.GetKeyDown (KeyCode.A)) {
+						if (transform.eulerAngles != (new Vector3 (30, 0, 0))) {
+								transform.eulerAngles = new Vector3 (30, 0, 0);
+								transform.position = new Vector3 (this.transform.position.x + CameraRotDisLocation,
+						                                this.transform.position.y,
+						                                this.transform.position.z - CameraRotDisLocation);
+				inverse=0;
+				non_inverse=1;
+				negetor =1;
+						}                          
+				}
+				//transform.eulerAngles.Set(30,0,0);
+		}
+}
---
+++ Top_down_project/Assets/SCripts/ClickPlay.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ClickPlay : MonoBehaviour {
+	//static public byte playerCount;
+	public Slider PlayerC;
+	public Button Starter;
+	public Text CharCountNumber;
+	 
+	//"
+	// Use this for initialization
+	void Start () {
+
+	}
+	public void SliderV ()
+	{STATICS.playerCounts = (byte)PlayerC.value;
+		CharCountNumber.text = PlayerC.value.ToString(); 
+	}
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("HELIPP");
+
+	}
+}
